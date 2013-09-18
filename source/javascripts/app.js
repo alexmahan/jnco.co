ec479e7e226c09f67c181d91cda2d9d89310d4ec
alexmahan <alexmahan@gmail.com>
2013-09-18 15:33:29 -0700

    cleaning up indentation in app.js

--- source/javascripts/app.js
+++ source/javascripts/app.js
@@ -1,69 +1,65 @@
 (function($) {
 
-    var JncoCam = {
-        init: function() {
-            this.jncoGrid.init();
-        },
+  var JncoCam = {
+    init: function() {
+      this.jncoGrid.init();
+    },
 
-        jncoGrid: {
+    jncoGrid: {
 
-            instagramAccessToken: '192731193.22c81c6.3d3f9b8ca1d24c2a8304767af4fbe6b3', // For 'alexshred420' user
+      instagramAccessToken: '192731193.22c81c6.3d3f9b8ca1d24c2a8304767af4fbe6b3', // For 'alexshred420' user
 
-            init: function() {
+      init: function() {
 
-                var $instagram = $('#instagram'),
-                    endpoint = 'https://api.instagram.com/v1/tags/jncocam/media/recent?access_token=' + this.instagramAccessToken + '';
+        var $instagram = $('#instagram'),
+          endpoint = 'https://api.instagram.com/v1/tags/jncocam/media/recent?access_token=' + this.instagramAccessToken + '';
 
-                $.ajax({
-                    crossDomain: true,
-                    dataType: 'jsonp',
-                    url: endpoint
+        $.ajax({
+          crossDomain: true,
+          dataType: 'jsonp',
+          url: endpoint
+        })
+        .done(function(data, status, jqXHR) {
+
+          if (status == 'success') {
+            $.each(data.data, function(index, image) {
+              var url = image.link,
+                imageUrl = image.images.standard_resolution.url,
+                caption = '';
+
+              // Create an <a> image wrapper
+              $imageWrapper = $('<a>')
+                .attr({
+                  'class': 'image',
+                  'href': url,
+                  'target': '_blank'
                 })
-                .done(function(data, status, jqXHR) {
+                .append(
+                  $('<img>').attr('src', imageUrl)
+                );
 
-                    if (status == 'success') {
-                        $.each(data.data, function(index, image) {
-                            var url = image.link,
-                                imageUrl = image.images.standard_resolution.url,
-                                caption = '';
+              // Append the username, if present
+              if (image.caption && image.caption.from.username) {
+                $imageWrapper.append(
+                  $('<small>')
+                    .attr('class', 'caption')
+                    .text(image.caption.from.username)
+                );
+              }
 
-                            // Create an <a> image wrapper
+              // Write it to the page
+              $instagram.append(
+                $imageWrapper
+              );
+            });
+          }
+        });
+      }
+    }
+  };
 
-                            $imageWrapper = $('<a>')
-                                .attr({
-                                    'class': 'image',
-                                    'href': url,
-                                    'target': '_blank'
-                                })
-                                .append(
-                                    $('<img>').attr('src', imageUrl)
-                                );
-
-                            // Append the caption, if present
-
-                            if (image.caption && image.caption.from.username) {
-                                $imageWrapper.append(
-                                    $('<small>')
-                                        .attr('class', 'caption')
-                                        .text(image.caption.from.username)
-                                );
-                            }
-
-                            // Write it to the page
-
-                            $instagram.append(
-                                $imageWrapper
-                            );
-                        });
-                    }
-                });
-
-            }
-        }
-    };
-
-    $(document).ready(function() {
-        JncoCam.init();
-    });
+  $(document).ready(function() {
+    JncoCam.init();
+  });
 
 })(jQuery);
